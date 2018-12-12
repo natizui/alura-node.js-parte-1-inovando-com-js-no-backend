e0821316f47119641d602679e85d27741205a285
natizui <izui.nm@gmail.com>
2018-12-12 17:26:51 -0200

    atividade 5 - nodemon

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const app = require('./src/config/custom-express');
 
 app.listen(3000, () => console.log('servidor rodando da porta 3000'));
 
+
+//$ npm install nodemon@1.18.4 --save-dev --save-exact
+
