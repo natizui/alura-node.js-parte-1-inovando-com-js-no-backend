ccc5906ce424c6d5c7ac9e11c2f224d37ffbba25
natizui <izui.nm@gmail.com>
2018-12-12 16:36:09 -0200

    atividade 4 - encapsulando rotas

--- server.js
+++ server.js
@@ -2,27 +2,3 @@ const app = require('./src/config/custom-express');
 
 app.listen(3000, () => console.log('servidor rodando da porta 3000'));
 
-app.get('/', (req, resp) => {
-    resp.send(`
-                <html>
-                    <head>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1>Casa do Código</h1>
-                    </body>
-                </html>
-            `);
-});
-app.get('/livros', (req, resp) => {
-    resp.send(`
-                <html>
-                    <head>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1>Listagem de Livros</h1>
-                    </body>
-                </html>
-            `);
-});
