084d6020aad14bde0d33edcb549319b357ff8cb5
natizui <izui.nm@gmail.com>
2018-12-12 17:51:44 -0200

    atividade 8 - templates dinâmicos

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -12,7 +12,21 @@ module.exports = (app) => {
                 `);
     });
     app.get('/livros', (req, resp) => {
-        resp.marko(require('../views/livros/lista/lista.marko'));
+        resp.marko(
+            require('../views/livros/lista/lista.marko'),
+            {
+                livros: [
+                    {
+                        id: 1,
+                        titulo: 'Fundamentos do Node'
+                    },
+                    {
+                        id: 2,
+                        titulo: 'Node Avançado'
+                    }
+                ]
+            }
+        );
     });
 };
 
