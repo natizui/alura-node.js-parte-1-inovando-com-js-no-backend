bbc73eec8f856966d61bbb599b8b776e5fc95828
natizui <izui.nm@gmail.com>
2018-12-12 17:44:54 -0200

    atividade 7 - marko

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -12,16 +12,7 @@ module.exports = (app) => {
                 `);
     });
     app.get('/livros', (req, resp) => {
-        resp.send(`
-                    <html>
-                        <head>
-                            <meta charset="utf-8">
-                        </head>
-                        <body>
-                            <h1>Listagem de Livros</h1>
-                        </body>
-                    </html>
-                `);
+        resp.marko(require('../views/livros/lista/lista.marko'));
     });
 };
 
