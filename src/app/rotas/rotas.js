ed9b8083904e1b69277b73f7fa31d8d118fcd9fa
natizui <izui.nm@gmail.com>
2018-12-13 09:21:33 -0200

    atividade 1 - banco de dados (sqlite3)

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,5 @@
+const db = require('../../config/database');
+
 module.exports = (app) => {
     app.get('/', (req, resp) => {
         resp.send(`
@@ -12,21 +14,14 @@ module.exports = (app) => {
                 `);
     });
     app.get('/livros', (req, resp) => {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ]
-            }
-        );
+        db.all('SELECT * FROM livros', (erro, resultados) => {
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+        });
     });
 };
 
